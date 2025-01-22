3e3591666104d0686a2c6d95ed41f45e4d672389
Toderean R. <raultoderean20@gmail.com>
2025-01-22 10:33:46 +0100

    Added footer - main screen

--- the-perfect-gift/src/app/app.component.ts
+++ the-perfect-gift/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FlowbiteService } from '../app/services/flowbite.service';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { ProductsComponent } from './components/products/products.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductsComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
